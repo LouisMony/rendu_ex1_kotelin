a75517d1ca92568a15ca53c6593a5da58a2a14ad
Louis Mony <73167166+LouisMony@users.noreply.github.com>
2022-06-14 12:24:20 +0200

    rendu

---
+++ app/src/main/java/com/example/iim_app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.radiobutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import com.example.iim_app.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
